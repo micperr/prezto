62f86c4871893f90027a38fbde7bb7fe75c33f3a
Michał Perliński <michal.perlinski@gmail.com>
2019-08-13 14:15:40 +0200

    Removed update_prezto alias function

--- contrib/micper/system.zsh
+++ contrib/micper/system.zsh
@@ -6,9 +6,3 @@ alias highlight='highlight -O ansi --syntax'
 function del() {
    mv $1 ~/.Trash
 }
-
-function update_prezto() {
-  cd $ZPREZTODIR
-  git pull
-  git submodule update --init --recursive
-}
