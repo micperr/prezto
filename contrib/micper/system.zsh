a42b817e0c3edf22288471b59daf87710ebb24bf
Michał Perliński <michal.perlinski@gmail.com>
2019-08-15 10:59:35 +0200

    Added system.zsh @@ alias and copy-prezto-os-zsh function

--- contrib/micper/system.zsh
+++ contrib/micper/system.zsh
@@ -1,8 +1,13 @@
 alias @='cd ~/Workspace'
+alias @@='cd ~/.zprezto/contrib/micper'
 alias reload='source ~/.zshrc'
 alias killit='killall -s 9'
 alias highlight='highlight -O ansi --syntax'
 
 function del() {
    mv $1 ~/.Trash
+}
+
+function copy-prezto-os-zsh() {
+   cp ~/.zprezto/contrib/micper/os.zsh.dist ~/.zprezto/contrib/micper/os.zsh
 }
