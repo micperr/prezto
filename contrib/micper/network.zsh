e8a64e3f9def7daaee7c2c37613ed3ffff88ad71
Michał Perliński <michal.perlinski@gmail.com>
2019-08-21 11:41:26 +0200

    Changed list ports alias name.

--- contrib/micper/network.zsh
+++ contrib/micper/network.zsh
@@ -2,4 +2,4 @@ alias devilnet-db-tunnel='ssh -f devilnet -L 8543:pgsql20.mydevil.net:5432 -N'
 alias ip="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 alias mountremote='sshfs $1 $2'
 alias mountremotedevilnet='sshfs devilnet:/home/micper/domains/micper.usermd.net Remote'
-alias listports='sudo lsof -i -P -n | grep LISTEN'
+alias lsports='sudo lsof -i -P -n | grep LISTEN'
